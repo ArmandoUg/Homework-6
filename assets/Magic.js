83313ad4dd47ffbd7fe98ea7f71af3d19a1a893a
Armando Ugalde <uarmando15@yahoo.com>
2022-05-14 16:25:00 -0700

    wrapped up logic for obtaining main card api data and adjusted html content for dynamic additons from api

--- assets/Magic.js
+++ assets/Magic.js
@@ -7,8 +7,12 @@ var limit;
 var statecode;
 
 var results = document.querySelector(".citymain");
-var cityname = document.querySelector(".searchname");
 var searchbtn = document.querySelector(".citysearch");
+var cityname = document.querySelector(".searchname");
+var stemp= document.querySelector(".temp")
+var shumidity= document.querySelector(".humidty");
+var wind = document.querySelector(".wind");
+
 // var icon= document.querySelector("");
  function getWeather () {
      fetch(weatherapi)
@@ -18,7 +22,7 @@ var searchbtn = document.querySelector(".citysearch");
      .then(function (apidata){
          console.log(apidata);
          var utcdets = apidata.current.dt;
-         var tzoffset= apidata.timezone_offset;
+        //  var tzoffset= apidata.timezone_offset;
          var date = new Date(utcdets * 1000);
          var presentdate = new Date(utcdets * 1000).toLocaleDateString("en-US")
          var hours = date.getHours();
@@ -26,6 +30,25 @@ var searchbtn = document.querySelector(".citysearch");
          var formattedtime= hours + `:` + minutes;
          console.log(formattedtime);
          console.log(presentdate);
+
+         var humid = apidata.current.humidity;
+
+         var temp = apidata.current.temp;
+         var ktoc = ((temp-273.15)*1.8)+32;
+         
+         var weatherdetails = apidata.current.weather[0].icon;
+         var icon = `http://openweathermap.org/img/wn/${weatherdetails}@3x.png`;
+
+         var windsped = Math.round(apidata.current.wind_speed * 10) / 10;
+         console.log(ktoc);
+
+         showmainforecast(presentdate, ktoc, humid, windsped, icon)
+
+         for (var i = 1; i < 5; i++) {
+             var weekutc = apidata.daily[i].dt;
+             var weekr= new Date(weekutc *1000);
+             var weekdatedata= weekr.toLocaleDateString();
+         }
      })
      .catch(function(err){
          console.log(err);
